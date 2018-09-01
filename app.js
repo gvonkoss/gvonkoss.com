8592e5a4b77452e88efac3ba91ca3881d5cbbd11
Gabrielle <gabivk@gmail.com>
2018-09-01 12:08:56 +0100

    add 404

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const path = require('path');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/about.html');
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(__dirname + '/404.html');
 });
 
 app.listen(3000, () => {
