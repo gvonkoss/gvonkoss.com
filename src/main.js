84c024ef161d7ff865df47a68e3aa13df21965b3
Gabrielle von Koss <gabivk@gmail.com>
2019-04-24 09:29:18 +0100

    clean up js

--- src/main.js
+++ src/main.js
@@ -1,49 +1,58 @@
-let rootStyle = document.documentElement.style;
+const rootStyle = document.documentElement.style;
 
 const changeColors = (root, color, outlineColor, textColor) => {
-  root.setProperty('--color', color);
-  root.setProperty('--outlineColor', outlineColor);
-  root.setProperty('--textColor', textColor);
-}
-let text = document.querySelector('input.text')
-let outline = document.querySelector('input.outline')
-let highlight = document.querySelector('input.highlight')
-
-let inputs = document.querySelectorAll('input');
-inputs.forEach(input => input.addEventListener('input', this));
-inputs.forEach(input => input.addEventListener('change', this));
-
-handleEvent = (e) => {
-  if (e.target.className === 'text') {
-    rootStyle.setProperty('--textColor', e.target.value)
-  } else if (e.target.className === 'outline') {
-    rootStyle.setProperty('--outlineColor', e.target.value)
-  } else if (e.target.className === 'highlight') {
-    rootStyle.setProperty('--color', e.target.value)
-  }
+	root.setProperty('--color', color);
+	root.setProperty('--outlineColor', outlineColor);
+	root.setProperty('--textColor', textColor);
 }
 
-let buttons = document.querySelectorAll('button');
 const randomize = () => {
-  let hex = '#'+Math.floor(Math.random()*16777215).toString(16);
-  console.log(hex);
-  return hex === '#ffffff' || hex.length !== 7 ? randomize() : hex;
+	let hex = `#${Math.floor(Math.random() * 16777215).toString(16)}`; //16777215 is #ffffff
+	return hex === '#ffffff' || hex.length !== 7 ? randomize() : hex;
 };
 
-buttons.forEach(button => button.addEventListener('click', e => {
-  let rootStyle = document.documentElement.style;
-  if (e.target.className === 'reset') {
-    changeColors(rootStyle, '#aeeedc', '#21d19f', '#158666');
-    text.value = '#158666';
-    outline.value = '#21d19f'
-    highlight.value = '#aeeedc'
-  } else if (e.target.className === 'random') {
-    let one = randomize();
-    let two = randomize();
-    let three = randomize();
-    changeColors(rootStyle, one, two, three);
-    text.value = one;
-    outline.value = two;
-    highlight.value = three;
-  }
-}))
+const original = () => ({
+	highlight: '#aeeedc',
+	outline: '#21d19f',
+	text: '#158666'
+});
+
+const random = () => ({
+	highlight: randomize(),
+	outline: randomize(),
+	text: randomize()
+});
+
+const set = (newColors) => {
+	let colors = newColors();
+	changeColors(rootStyle, colors.highlight, colors.outline, colors.text);
+	inputs[0].value = colors.highlight;
+	inputs[1].value = colors.outline;
+	inputs[2].value = colors.text;
+}
+
+this.handleEvent = (e) => {
+	if (e.target.className === 'text') {
+		rootStyle.setProperty('--textColor', e.target.value);
+	} else if (e.target.className === 'outline') {
+		rootStyle.setProperty('--outlineColor', e.target.value);
+	} else if (e.target.className === 'highlight') {
+		rootStyle.setProperty('--color', e.target.value);
+	} else if (e.target.className === 'reset') {
+		set(original);
+	} else if (e.target.className === 'random') {
+		set(random);
+	}
+}
+
+const inputs = Array.from(document.querySelectorAll('input'), input => {
+	input.addEventListener('input', this);
+	input.addEventListener('change', this);
+	return input;
+});
+
+const buttons = Array.from(document.querySelectorAll('button'), button => {
+	button.addEventListener('click', this);
+});
+
+window.addEventListener('onunload', set(original));
