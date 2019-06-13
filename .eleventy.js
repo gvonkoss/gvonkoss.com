5b44be9f5f90133bbef383e8a2117fc1414b8495
Gabrielle <gabivk@gmail.com>
2019-06-13 10:44:45 +0200

    fix images

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
-module.exports = (config) => ({
-	dir: { input: 'src', output: 'dist', includes: '_includes' },
-	templateFormats: ['njk', 'md', 'css'],
-	htmlTemplateEngine: 'njk'
-})
+module.exports = (config) => {
+	config.addPassthroughCopy('src/images');
+
+	return {
+		dir: { input: 'src', output: 'dist', includes: '_includes' },
+		templateFormats: ['njk', 'md', 'css'],
+		htmlTemplateEngine: 'njk'
+	}
+}
