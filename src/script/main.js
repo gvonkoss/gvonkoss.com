1726d53885babe990e9877fe51ab92fc86fab232
Gabrielle von Koss <gabivk@gmail.com>
2019-06-27 13:02:32 +0200

    correct background size (#3)
    * correct background size
    
    * rogue console log

--- src/script/main.js
+++ src/script/main.js
@@ -40,8 +40,8 @@ const setOne = (property, value) => {
 		let size = property === 'striped' ? 'none' : '4px 4px';
 		property = 'background';
 
-		root.setProperty('--background-size', size);
 		set('size', size);
+		root.setProperty('--background-size', get('size'));
 	}
 
 	root.setProperty(`--${property}`, value);
@@ -55,7 +55,7 @@ const setAll = (colors) => {
 
 	if(colors.highlight === original.highlight) {
 		set('background', 'var(--dotted)');
-		set('--background-size', '4px 4px');
+		set('size', '4px 4px');
 	}
 
 	fill();
