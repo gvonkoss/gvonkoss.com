afd0c6f0f82bd448eb67c782b76d7414ec322018
Gabrielle von Koss <gabivk@gmail.com>
2020-09-10 17:17:32 +0100

    self host fonts..ish

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const { DateTime } = require('luxon');
 
 module.exports = (config) => {
 	config.addPassthroughCopy('src/images');
+	config.addPassthroughCopy('src/fonts');
 
 	config.addFilter('readableDate', dateObj => {
 		return DateTime.fromJSDate(dateObj, {
